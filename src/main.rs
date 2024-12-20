b6d4482794fc83bffd8f2b8873509948e97855c2
iChaplain <interrogator-chaplain@pm.me>
2024-12-20 16:18:51 +0200

    Fetch electricity prices once an hour

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
-use actix_web::web::ServiceConfig;
+use std::sync::Mutex;
+use std::time::{Duration, Instant};
+
+use actix_web::web::{Data, ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 
 mod index;
 mod price;
 
 use index::get_index;
-use price::get_price;
+use price::{get_price, Prices};
+
+const SYNC_PERIOD: Duration = Duration::from_secs(3600);
+
+struct PriceSync {
+    last_sync: Instant,
+    prices: Prices,
+}
+
+struct AppState {
+    price_sync: Mutex<PriceSync>,
+}
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let app_data = Data::new(AppState {
+        price_sync: Mutex::new(PriceSync {
+            last_sync: Instant::now() - 2 * SYNC_PERIOD,
+            prices: Prices {
+                prices: Vec::new(),
+                dates: Vec::new(),
+            },
+        }),
+    });
+
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(get_index)
-        .service(get_price);
+        cfg.app_data(app_data).service(get_index).service(get_price);
     };
 
     Ok(config.into())
--- src/price.rs
+++ src/price.rs
@@ -1,6 +1,66 @@
-use actix_web::{get, http::header::CONTENT_TYPE, HttpResponse};
+use std::time::Instant;
+
+use actix_web::{error, get, web, Result};
+use chrono::prelude::*;
+use serde::{Deserialize, Serialize};
+use tracing::info;
+
+use crate::{AppState, SYNC_PERIOD};
+
+#[derive(Serialize, Clone)]
+pub struct Prices {
+    pub prices: Vec<f32>,
+    pub dates: Vec<String>,
+}
+
+#[derive(Deserialize)]
+struct ApiPrices {
+    license_info: String,
+    unix_seconds: Vec<i64>,
+    price: Vec<f32>,
+    unit: String,
+    deprecated: bool,
+}
 
 #[get("/price")]
-pub async fn get_price() -> HttpResponse {
-    HttpResponse::Ok().append_header((CONTENT_TYPE, "application/json")).finish()
+pub async fn get_price(data: web::Data<AppState>) -> Result<web::Json<Prices>> {
+    let mut price_sync = data.price_sync.lock().map_err(|e| error::ErrorInternalServerError(e.to_string()))?;
+    let now = Instant::now();
+
+    if  price_sync.last_sync + SYNC_PERIOD > now {
+        info!("returning existing prices");
+        return Ok(web::Json(price_sync.prices.clone()));
+    }
+
+    info!("requesting new prices");
+
+    let text_response = reqwest::get("https://api.energy-charts.info/price?bzn=DE-LU")
+        .await
+        .map_err(|e| error::ErrorInternalServerError(e.to_string()))?
+        .text()
+        .await
+        .map_err(|e| error::ErrorInternalServerError(e.to_string()))?;
+
+    let json = serde_json::from_str::<ApiPrices>(&text_response)
+        .map_err(|e| error::ErrorInternalServerError(e.to_string()))?;
+
+    let mut dates = Vec::new();
+    for date in json.unix_seconds {
+        dates.push(
+            DateTime::from_timestamp(date, 0)
+                .ok_or_else(|| error::ErrorInternalServerError("date conversion failed"))?
+                .format("%Y-%m-%d %H:%M:%S")
+                .to_string(),
+        );
+    }
+
+    let new_prices = Prices {
+        prices: json.price,
+        dates,
+    };
+
+    price_sync.prices = new_prices.clone();
+    price_sync.last_sync = now;
+
+    Ok(web::Json(new_prices))
 }
